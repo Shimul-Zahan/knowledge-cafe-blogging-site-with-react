436430d5d9e7e5793bfaef59b14c39b8dfa61680
AlgoWebBot <shimulzahan636@gmail.com>
2023-09-12 02:11:08 +0600

    navbar add

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './Components/Header/Header'
+
+function App() {
+
+  return (
+    <>
+      <div className='container mx-auto py-4 px-4'>
+        <Header />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ProfileImage from '../../assets/images/boy2.png'
+
+const Header = () => {
+    return (
+        <div className="navbar border-b-2">
+            <div className="flex-1">
+                <a className="btn btn-ghost normal-case text-2xl">Knowledge Cafe</a>
+            </div>
+            <div className="flex-none">
+                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                    <div className="w-20 rounded-full">
+                        <img src={ProfileImage} />
+                    </div>
+                </label>
+            </div>
+        </div>
+    )
+}
+
+export default Header
